7a4a3c6c30b51c4a499338a5df69ac463ddbf403
Alberto <albertojunior848423803@gmail.com>
2025-01-18 19:48:39 +0300

    first commit

---
+++ init.lua
@@ -0,0 +1,67 @@
+-- This file bootstraps Lazy.nvim and configures Mason and gopls for Go development.
+
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Validate that Lazy.nvim is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
+require "lazy_setup"
+require "polish"
+
+-- Import Mason, Mason-LSPconfig, and Mason-Null-LS
+local mason_lspconfig = require("mason-lspconfig")
+local mason_null_ls = require("mason-null-ls")
+local lspconfig = require("lspconfig")
+
+-- Initialize Mason
+require("mason").setup()
+
+-- Setup Mason-LSPconfig for LSP installation
+mason_lspconfig.setup({
+    ensure_installed = { "gopls" },  -- Ensure gopls is installed
+    automatic_installation = true,    -- Automatically install LSPs
+})
+
+-- Setup Mason-Null-LS for formatters/linters installation
+mason_null_ls.setup({
+  ensure_installed = {
+    "stylua",              -- Lua formatter
+    "goimports",           -- Go imports formatter
+    "gofumpt",             -- Go stricter formatter
+    "goimports-reviser",   -- Go imports reviser tool
+    -- Add other tools you want to install
+  },
+})
+
+-- Configure gopls (Go LSP)
+lspconfig.gopls.setup({
+    on_attach = function(client, bufnr)
+        -- Enable formatting on save
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format()
+            end,
+        })
+    end,
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true, -- Enable analysis for unused params
+            },
+            staticcheck = true,    -- Enable static code analysis
+        },
+    },
+})
+
---
+++ lua/lazy_setup.lua
@@ -0,0 +1,64 @@
+require("lazy").setup({
+  {
+    "AstroNvim/AstroNvim",
+    version = "^4", -- Remove version tracking to elect for nightly AstroNvim
+    import = "astronvim.plugins",
+    opts = {
+      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
+      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
+      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
+      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
+      update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
+    },
+  },
+  { import = "community" },
+  { import = "plugins" },
+
+  -- Add the null-ls plugin here
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.gofumpt,
+          null_ls.builtins.formatting.goimports_reviser,
+          null_ls.builtins.formatting.golines,
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({
+              group = augroup,
+              buffer = bufnr,
+            })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+              end,
+            })
+          end
+        end,
+      })
+    end
+  },
+}, {
+  -- Additional LazyConfig options here
+  install = { colorscheme = { "astrotheme", "habamax" } },
+  ui = { backdrop = 100 },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
